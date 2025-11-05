22d3c0d3f399d55df38769adeb166244f7510bcf
bhautik <bhootbhautik@gmail.com>
2025-11-05 11:12:35 +0530

    change repo name

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? '/portfolio' : '',
-  assetPrefix: isProd ? '/portfolio/' : '',
+  basePath: isProd ? '/Portfolio' : '',
+  assetPrefix: isProd ? '/Portfolio/' : '',
   images: {
     unoptimized: true,
   },
