dbccccb84caf294a9cffe691e6ee8f73c0c9e18c
bhautik <bhootbhautik@gmail.com>
2025-11-05 10:49:35 +0530

    replace code

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/Portfolio',  // Add this line
-  assetPrefix: '/Portfolio',  // Add this line too
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio',
   images: {
-    unoptimized: true  // Required for static export
+    unoptimized: true
   }
 }
 
-module.exports = nextConfig
+export default nextConfig
